c11921fbad8fd559a0384c27fb18e7b10a12a16b
allen <ywl_allen@163.com>
2021-07-11 19:58:40 +0800

    加入多个页面

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@
  * @Author: ywl
  * @LastEditors: ywl
  */
-function App() {
-  return <div>hello world</div>;
-}
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import Exception from "@/views/Exception";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </Router>
+    )
+  }
+}
---
+++ src/views/Exceptions/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Exceptions extends Component {
+  render() {
+    return <div>Exceptions</div>;
+  }
+}
